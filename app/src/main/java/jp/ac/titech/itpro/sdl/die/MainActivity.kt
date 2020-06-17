cac2d3c4d60e96c42572e94ae985568bce355a5f
Durun <negiigainuki@gmail.com>
2020-06-18 00:17:01 +0900

    convert Java to Kotlin

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/die/MainActivity.kt
@@ -0,0 +1,71 @@
+package jp.ac.titech.itpro.sdl.die
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), OnSeekBarChangeListener {
+    private val renderer = SimpleRenderer()
+    private val cube = Cube()
+    private val pyramid = Pyramid()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
+        setContentView(R.layout.activity_main)
+        seekbar_x.max = 360
+        seekbar_y.max = 360
+        seekbar_z.max = 360
+        seekbar_x.setOnSeekBarChangeListener(this)
+        seekbar_y.setOnSeekBarChangeListener(this)
+        seekbar_z.setOnSeekBarChangeListener(this)
+        renderer.setObj(cube)
+        gl_view.setRenderer(renderer)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+        gl_view.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+        gl_view.onPause()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        Log.d(TAG, "onCreateOptionsMenu")
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d(TAG, "onOptionsItemSelected")
+        when (item.itemId) {
+            R.id.menu_cube -> renderer.setObj(cube)
+            R.id.menu_pyramid -> renderer.setObj(pyramid)
+        }
+        return true
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+        when (seekBar.id) {
+            R.id.seekbar_x -> renderer.rotateObjX(progress.toFloat())
+            R.id.seekbar_y -> renderer.rotateObjY(progress.toFloat())
+            R.id.seekbar_z -> renderer.rotateObjZ(progress.toFloat())
+        }
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar) {}
+    override fun onStopTrackingTouch(seekBar: SeekBar) {}
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+}
